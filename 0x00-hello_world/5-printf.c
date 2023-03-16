db9b015e17d4d3d1c90170c448f7f343e889da83
appleex-mhd <mr.mhdhilali@email.com>
2023-03-16 04:12:18 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - A program that print whit a printf function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
