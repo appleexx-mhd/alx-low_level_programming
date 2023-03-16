0dee04be34ab3e576c466fb7e52d588a3776931b
appleex-mhd <mr.mhdhilali@email.com>
2023-03-16 08:33:57 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include <unistd.h>
 /**
  * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
+ *
+ * Return: 1  (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
