49b22dfda02887e9df20e8bf7b7d715d1fdc7880
appleex-mhd <mr.mhdhilali@email.com>
2023-03-16 07:42:07 -0700

    Unix is simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
- * main - A program that print out the last part of quot in the standard error
- * Return: 1 if success
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if success.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
-
 }
