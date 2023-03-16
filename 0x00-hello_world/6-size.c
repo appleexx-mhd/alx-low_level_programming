d20033c0eabd3954e5b3e38ec771a52e372d69cd
appleex-mhd <mr.mhdhilali@email.com>
2023-03-16 05:50:06 -0700

    Size of the data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,6 @@
  * main - A program that print the size of the data type
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int intType;
@@ -11,13 +10,11 @@ int main(void)
 	long int longintType;
 	long long int longlongType;
 	char charType;
-	
-	printf("Size of a char: %zu byte(s)\n",sizeof(charType));
-	printf("Size of an int: %zu byte(s)\n",sizeof(intType));
-	printf("Size of a long int: %zu byte(s)\n",sizeof(longintType));
-	printf("Size of a long long int: %zu byte(s)\n",sizeof(longlongType));
-	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-
-	return (0);
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+return (0);
 
 }
