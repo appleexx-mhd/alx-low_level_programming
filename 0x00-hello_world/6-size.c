440040e1c9ea0e744d983b74f631db8626a063b2
appleex-mhd <mr.mhdhilali@email.com>
2023-03-16 04:49:15 -0700

    Size of type data

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - A program that print the size of the data type
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int intType;
+	float floatType;
+	long int longintType;
+	long long int longlongType;
+	char charType;
+	
+	printf("Size of char: %zu byte(s)\n",sizeof(charType));
+	printf("Size of int: %zu byte(s)\n",sizeof(intType));
+	printf("Size of long int: %zu byte(s)\n",sizeof(longintType));
+	printf("Size of long long int: %zu byte(s)\n",sizeof(longlongType));
+	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
+
+	return (0);
+
+}
