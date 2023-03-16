06c596a9b22e84bcff0a8b84aa505ed66887d67d
appleex-mhd <mr.mhdhilali@email.com>
2023-03-16 07:32:34 -0700

    Unix is simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 65);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
 	return (1);
 
 }
