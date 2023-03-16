d97be6c809c8fd82456b31e61e8b906818a9cf45
appleex-mhd <mr.mhdhilali@email.com>
2023-03-16 03:45:25 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 /**
- * main - Entry point
- *
+ * main - a program that pritns a line with puts function
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	char C[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(C);
+
 	return (0);
 }
