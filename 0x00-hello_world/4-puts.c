49596707b8024f0a9d4a56f5910799cf1497670c
appleex-mhd <mr.mhdhilali@email.com>
2023-03-16 03:31:35 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
-int main(void){
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	char C[] = "\"Programming is like building a multilingual puzzle";
 	puts(C);
 	return (0);
