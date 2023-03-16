ddd14b7b384a8395fa835d23a9c82bcf75e932e9
appleex-mhd <mr.mhdhilali@email.com>
2023-03-16 03:21:58 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
-#include<string.h>
 int main(){
-	char str1;
-	strcpy(str1, "\"Programming is like building a multilingual puzzle");
-	puts(str1);
+	char C[]= "\"Programming is like building a multilingual puzzle";
+	puts(C);
 	return(0);
 }
