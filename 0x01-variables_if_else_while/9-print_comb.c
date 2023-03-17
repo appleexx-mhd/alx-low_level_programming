5ec4727b969047340202e8c2696e95991d0da358
appleex-mhd <mr.mhdhilali@email.com>
2023-03-17 03:35:19 -0700

    Patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - prints all possible combination of single digit numbers
+ (*
+  * Return: 0 (Success)
+  */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	return (0);
+}
