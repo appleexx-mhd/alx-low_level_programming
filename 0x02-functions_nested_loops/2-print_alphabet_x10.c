7a495eba73e526bfa29fa50d8832d6e49075281f
appleexx-mhd <mr.mhdhilali@email.com>
2023-03-21 11:42:18 +0000

    GOO fast

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,10 @@ void print_alphabet_x10(void)
 	char C = 'a';
 	int i = 0;
 
-	for (i = 0 ; i < 10 ; i++ )
+	for (i = 0 ; i < 10 ; i++)
 	{
 		C = 'a';
-		while ( C <= 'z')
+		while (C <= 'z')
 		{
 			_putchar(C);
 			C++;
