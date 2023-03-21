8425cc191819471e7fe67243ba96c894af946e59
appleexx-mhd <mr.mhdhilali@email.com>
2023-03-21 17:32:26 +0000

    Count fabonacci numebers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count = 0; i < 50; i++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (i == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
