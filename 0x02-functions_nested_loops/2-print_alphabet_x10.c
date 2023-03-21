6cd8d5528b1059f81401e5cdfdd0ef85a1d38a06
appleexx-mhd <mr.mhdhilali@email.com>
2023-03-21 11:32:21 +0000

    Go fast

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10 times the alphabet in lowercese
+ */
+void printf_alphabet_x10(void)
+{
+	char C = 'a';
+	int i = 0;
+
+	for (i = 0 ; i < 10 ; i++ )
+	{
+		C = 'a';
+		while ( C <= 'z')
+		{
+			_putchar(C);
+			C++;
+		}
+		_putchar('\n');
+	}
+}
