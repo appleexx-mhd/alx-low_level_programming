ac510ce4f7c757e6cdb09393c939eb8af822788b
appleexx-mhd <mr.mhdhilali@email.com>
2023-03-21 17:00:52 +0000

    Long long

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,11 +8,11 @@ void times_table(void)
 	int j;
 	int k;
 
-	for (i = 0 ; i < 10 ; i ++)
+	for (i = 0 ; i < 10 ; i++)
 	{
 		for (j = 0 ; j < 10 ; j++)
 		{
-			k = i*j;
+			k = i * j;
 			if (j == 0)
 			{
 				_putchar(k + '0');
