3ae660663f78d3bc91439ee694ceaea817192aff
appleexx-mhd <mr.mhdhilali@email.com>
2023-03-21 13:10:56 +0000

    Isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - cheks for alphabetic caracters
+ * Return: 1 if c is a letter ,0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+}
