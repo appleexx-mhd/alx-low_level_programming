5f9096affa9f80174d74ec28402558a4a00cb052
appleexx-mhd <mr.mhdhilali@email.com>
2023-03-21 10:54:57 +0000

    Main

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#inlclude "main.h"
+/**
+ * print_alphabet - print alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char C = 'a';
+	while (C <= 'z')
+	{
+		_putchar(C);
+		C++;
+	}
+	_putchar('\n');
+}
