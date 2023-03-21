04699db402f09b393f5449caadaa0383fe1941bc
appleexx-mhd <mr.mhdhilali@email.com>
2023-03-21 13:28:22 +0000

    Go blah

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * @c: the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */
-int _isalpha(int c)
+int _isalpha(int c);
 {
 	if ((c >= 'a' && c <= 'z')||(c >= 'A' && c <= 'Z'))
 	{
