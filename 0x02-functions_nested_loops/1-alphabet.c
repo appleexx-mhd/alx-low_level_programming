33bdb29a995cfd96ffa1f026c6e0a73f5a0ac10b
appleex-mhd <mr.mhdhilali@email.com>
2023-03-21 03:20:01 -0700

    Good

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+}
+
+
