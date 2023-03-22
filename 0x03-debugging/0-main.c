ed8288b3b999a941138d2f9128905c19e8d4c3ad
appleexx-mhd <mr.mhdhilali@email.com>
2023-03-22 02:49:57 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,12 @@
 #include "main.h"
 /**
- * main - test a prototype functions
- *
- * Return: Always 0 (Succcess)
- */
+* main - Test function for positive or negative
+* Return: 0
+*/
 int main(void)
 {
-	int i;
-
-	i = 0;
-
-	positive_or_negative(i);
-	return (0);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
