b54195dc72e433132822646fd9dcb089412da01d
appleexx-mhd <mr.mhdhilali@email.com>
2023-04-27 08:58:46 -0700

    Update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "lists.h"
+
 /**
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
@@ -19,5 +20,6 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		s++;
 	}
+
 	return (s);
 }
