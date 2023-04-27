52f4e63a36fd17ffd536204ebe4c7111a108b5e3
appleexx-mhd <mr.mhdhilali@email.com>
2023-04-27 08:11:07 -0700

    Update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t z = 0;
+	size_t s = 0;
 
 	while (h)
 	{
@@ -17,7 +17,8 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		z++;
+		s++;
 	}
-	return (z);
+
+	return (s);
 }
