f03d5fd8190983af968441cba260c7e466012cbf
appleexx-mhd <mr.mhdhilali@email.com>
2023-05-02 08:36:57 -0700

    Update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -19,7 +19,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(tmp);
 		return (1);
 	}
-	while (i < index -1)
+	while (i < index - 1)
 	{
 		if (!tmp || !(tmp->next))
 			return (-1);
