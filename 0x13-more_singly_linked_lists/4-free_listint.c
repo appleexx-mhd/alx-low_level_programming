d75a00b18a00f027289805a1182a543f1537cbde
appleexx-mhd <mr.mhdhilali@email.com>
2023-05-02 06:28:23 -0700

    Update

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint - function that frees a linked list
+ * @head: listint_t to be frees
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head == NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
