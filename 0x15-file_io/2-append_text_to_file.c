ac0e841fee919ff1ba7a6e436542165c2e76598a
appleexx-mhd <mr.mhdhilali@email.com>
2023-05-08 08:22:29 -0700

    Update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,13 +9,11 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int file;
 	int context;
-	int i;
+	int i = 0;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		return (-1);
-	else
+	if (text_content != NULL)
 	{
 		while (text_content[i] != '\0')
 			i++;
