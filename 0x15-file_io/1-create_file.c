cd5006526ab2cdd90b67382e3b5836b21c9353fd
appleexx-mhd <mr.mhdhilali@email.com>
2023-05-08 07:55:02 -0700

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
  * create_file - function that create a file
- * @filename - the name if the file to be created
- * @text_content - is a NULL terminated string to write to the file
+ * @filename: the name if the file to be created
+ * @text_content: is a NULL terminated string to write to the file
  * Return: 1 On success, or -1 On failure
  */
 int create_file(const char *filename, char *text_content)
 {
 	int file;
 	int context;
-	int i;
+	int i = 0;
 
 	if (filename == NULL)
 		return (-1);
